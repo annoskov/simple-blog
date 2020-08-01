895f883cf8ff55ad85cea5b5d3c751a28c31d37c
antonnoskov <anoskov@dm-matrix.com>
2020-08-01 13:15:39 +0300

    Added slug generation for markdown posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,19 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, '.md');
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+
+}
